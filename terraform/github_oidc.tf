d89dfa4fe81b5c5be239c5572b5c703fba8511c8
Jeneel Dumasia <jeneel.dumasia@itara.io>
2026-06-19 00:56:38 +0530

    feat: add GitHub Actions OIDC role for securely deploying secrets

---
+++ terraform/github_oidc.tf
@@ -0,0 +1,45 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "ShipZenGitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          }
+          StringLike = {
+            # Allows any repo in your GitHub account, avoiding the DeployHub/ShipZen naming conflict!
+            "token.actions.githubusercontent.com:sub" : "repo:jeneeldumasia/*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Grant the GitHub Actions role cluster-admin access to EKS
+resource "aws_eks_access_entry" "github_actions" {
+  cluster_name  = module.eks.cluster_name
+  principal_arn = aws_iam_role.github_actions.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "github_actions" {
+  cluster_name  = module.eks.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_iam_role.github_actions.arn
+  access_scope {
+    type = "cluster"
+  }
+}
