8ce953357b894c923b5149083cd00d4e1c40cbc0
Jeneel Dumasia <jeneel.dumasia@itara.io>
2026-06-18 23:39:14 +0530

    fix(ui): import CredentialsProvider to fix build error

--- ui/src/auth.ts
+++ ui/src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 // Use GitHub if configured, otherwise use Stub Credentials
 const providers = []
